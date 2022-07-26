fc084830c546f7608213b043d55e3d3856749576
pkskdl <pkskadakkal@gmail.com>
2022-07-26 12:14:11 +0530

    Update dag_full_load.py

--- dag_full_load.py
+++ dag_full_load.py
@@ -5,19 +5,17 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 
 version = "0.1.0"
 
-default_args = {
-    '-t': 'jsontopic',
-    '-s': 'full_load',
-    '-l': 'Warn',
-    '-b': 'localhost',
-    '-a': 'aes256'
-    }
+topic='jsontopic'
+load_type='full_load'
+log_type='Warn'
+broker='localhost:29092'
+encryption_algorithm='aes256'
 with DAG(
         dag_id='ftp_full_load',
         schedule_interval=None,
         start_date=datetime.datetime.now() - datetime.timedelta(days=1),
-        dagrun_timeout=datetime.timedelta(minutes=60),
-        default_args=default_args
+        dagrun_timeout=datetime.timedelta(minutes=60)
+        
 ) as dag:
     start = DummyOperator(
         task_id='start',
@@ -31,7 +29,7 @@ with DAG(
         namespace='service-monitoring',
         name="ftp_fullload",
         image="priyesh2020/dagtest2:latest",
-        arguments=['-t=jsontopic','-s=full_load','-l=Warn','-b=localhost','-a=aes256'],
+        arguments=[f'-t={topic}',f'-s={load_type}',f'-l={log_type}',f'-b={broker}',f'-a={encryption_algorithm}'],
         task_id="ftp_fullload",
         get_logs=True,
         image_pull_policy='Always',
