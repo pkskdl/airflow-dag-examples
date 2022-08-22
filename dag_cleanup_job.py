36fa26c9696a792d7ea61c1b265ddaa035f55cb9
pkskdl <pkskadakkal@gmail.com>
2022-08-22 14:29:51 +0530

    Update dag_cleanup_job.py

--- dag_cleanup_job.py
+++ dag_cleanup_job.py
@@ -6,7 +6,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 version = "0.1.0"
 
 with DAG(
-        dag_id='ftp_cleanup',
+        dag_id='ftp_cleanup1',
         schedule_interval=None,
         start_date=datetime.datetime.now() - datetime.timedelta(days=1),
         dagrun_timeout=datetime.timedelta(minutes=60),
@@ -18,14 +18,14 @@ with DAG(
     end = DummyOperator(
         task_id='end',
     )
-    ftp_cleanup = KubernetesPodOperator(
+    ftp_cleanup1 = KubernetesPodOperator(
         namespace='service-monitoring',
-        name="ftp_cleanup",
+        name="ftp_cleanup1",
         image="priyesh2020/cleanup:latest",
         arguments=['--ho=ipaddress','-u=username','-p=pswd','-dp=/test'],
-        task_id="ftp_cleanup",
+        task_id="ftp_cleanup1",
         get_logs=True,
         image_pull_policy='Always',
         dag=dag
     )
-start >> ftp_cleanup >> end
+start >> ftp_cleanup1 >> end
